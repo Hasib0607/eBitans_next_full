b4f3433dffc4054e5cb43aad278408a107c404d2
jewel1269 <mohammadjewel4k5@gmail.com>
2024-08-19 11:21:11 +0600

    error page complete

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,59 @@
-const loading = () => {
+import { getSubdomainName } from "@/lib";
+import getUrl from "@/utils/get-url";
+
+const loading = async () => {
+
+  const url = getUrl();
+  const data = await getSubdomainName(url);
+  const { design } = data;
+
+  const No_Of_Loading_Spiner = design?.hero_slider || 1; 
+
+  const renderLoader = () => {
+    switch(No_Of_Loading_Spiner) {
+      case 1:
+        
+        return (
+          <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-violet-600"></div>
+        );
+      case 2:
+      
+        return (
+          <div className="w-16 h-16 bg-violet-600 animate-spin-slow transform rotate-45"></div>
+        );
+      case 3:
+     
+        return (
+          <div className="relative flex items-center justify-center">
+            <div className="w-16 h-16 bg-violet-600 rounded-full animate-ping absolute"></div>
+            <div className="w-8 h-8 bg-violet-800 rounded-full"></div>
+          </div>
+        );
+      case 4:
+      
+        return (
+          <div className="w-16 h-16 border-4 border-t-4 border-violet-600 border-t-transparent rounded-full animate-spin"></div>
+        );
+      case 5:
+       
+        return (
+          <div className="flex space-x-2">
+            <div className="w-4 h-4 bg-violet-600 rounded-full animate-bounce"></div>
+            <div className="w-4 h-4 bg-violet-600 rounded-full animate-bounce delay-200"></div>
+            <div className="w-4 h-4 bg-violet-600 rounded-full animate-bounce delay-400"></div>
+          </div>
+        );
+      default:
+        
+        return (
+          <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-violet-600"></div>
+        );
+    }
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-600"></div>
+      {renderLoader()}
     </div>
   );
 };
--- components/wrongUrl.jsx
+++ components/wrongUrl.jsx
@@ -8,7 +8,7 @@ const WrongUrl = () => {
     return (
         <div className="flex lg:flex-row flex-col justify-center items-center sm:container px-5 gap-10 lg:h-screen my-20 lg:my-0 w-full">
             <div className="w-full flex flex-col justify-center items-center">
-                <img className="h-auto" src={img1} alt="" />
+                <img className="h-auto" src={img1.src} alt="" />
                 <div className='md:text-[40px] text-2xl flex flex-col gap-2'>
                     <p>Something went</p>
                     <p className='font-bold'>Wrong <span className='font-normal'>or</span> Incorrect </p>
@@ -16,7 +16,7 @@ const WrongUrl = () => {
                 </div>
             </div>
             <div className="w-full mx-auto">
-                <img className="h-auto" src={img2} alt="" />
+                <img className="h-auto" src={img2.src} alt="" />
             </div>
         </div>
     );
