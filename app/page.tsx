6ee73fcdbc6dfe517328169edac91c8e12d80d44
ninjastorm24 <siamabdullah1100@gmail.com>
2024-08-26 13:40:25 +0600

    husky format

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export async function generateMetadata() {
   const url = getUrl();
   const { headersetting } = await getSubdomainName(url, "headersetting");
   const websiteName = capitalizeFirstLetter(headersetting?.website_name);
-  
+
   return {
     title: `${websiteName} | Home`,
     icons: { icon: imgUrl + headersetting?.favicon },
