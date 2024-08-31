97461f7ba4a4abe474bedbf475c32bd931526f20
ninjastorm24 <siamabdullah1100@gmail.com>
2024-08-31 12:38:38 +0600

    blog page created

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "admin.ebitans.com",
+      },
+    ],
+  },
+};
 
 const analyzerConfig = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
