8ad4e4ae30e57ab10fba73e32e4d3eba23d9fab1
ninjastorm24 <siamabdullah1100@gmail.com>
2024-09-01 17:50:03 +0600

    project running

--- utils/get-url.tsx
+++ utils/get-url.tsx
@@ -4,14 +4,10 @@ const getUrl = () => {
   const headersList = headers();
   const host = headersList.get("host");
   const forwardedPath = headersList.get("x-forwarded-path") || "";
-
   let url = `${host}${forwardedPath}`;
-
-  // Remove 'www.' if it exists in the URL
   if (url.includes("www.")) {
     url = url.replace("www.", "");
   }
-  // www.gadgetghor.shop -> gadgetghor.shop
   return url;
 };
 
