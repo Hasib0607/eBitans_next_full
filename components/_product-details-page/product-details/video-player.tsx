db3c94f12c90c979d597cbad5c7698d3d2533c6c
WAVE BOX <89796889+wavebox0607@users.noreply.github.com>
2024-11-28 16:44:48 +0600

    dsbahs db

--- components/_product-details-page/product-details/video-player.tsx
+++ components/_product-details-page/product-details/video-player.tsx
@@ -1,7 +1,7 @@
 const VideoPlayer = ({ videoUrl }: { videoUrl: string }) => {
   // Convert the normal YouTube URL to an embeddable one
-  const videoId = videoUrl?.split("v=")[1]; // Extract video ID from the URL
-  const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+  const videoId = videoUrl?.split("embed/")[1]; // Extract video ID from the URL
+  const embedUrl = `https://youtube.com/embed/${videoId}?autoplay=1`;
 
   return (
     <div className="video-container">
