a4018ec2dd44886458aae54779b539bd94877d1f
WAVE BOX <89796889+wavebox0607@users.noreply.github.com>
2024-12-24 17:38:40 +0600

    zarex color add

--- components/card/card58.tsx
+++ components/card/card58.tsx
@@ -68,16 +68,12 @@ const Card58 = ({ item }: any) => {
     button1,
     button1_color,
     button1_bg_color,
-<<<<<<< HEAD
-  } = data?.custom_design?.best_sell_product?.[0] || data?.custom_design?.product?.[0] || {};
-=======
   } =
     data?.custom_design?.best_sell_product?.[0] ||
     data?.custom_design?.product?.[0] ||
     data?.custom_design?.feature_product?.[0] ||
     data?.custom_design?.new_arrival?.[0] ||
     {};
->>>>>>> b82255afce0e7e256aa56c7028cf18eaaf98e6c2
 
   const styleCss = `
     .searchHover:hover {
