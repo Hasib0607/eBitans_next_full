7aed0e1571e6cecc6dbf5f1a11c9ac7a1294b6b6
Ashikuzzaman Moon <ashikuzzamanbd.hitcse2020@gmail.com>
2025-01-02 19:33:40 +0600

    final watch time

--- utils/customizeDesign.js
+++ utils/customizeDesign.js
@@ -102,7 +102,7 @@ export const customizeSingleProductPage = [
   },
   // for watch-time-bd customize text show in details page 28, store id:10064
   {
-    id: 8590,
+    id: 10064,
     text_bangla: true,
     customize_text_show_for_watchtime_1: (
       <div>
